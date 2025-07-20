2249a905aeb2d06c026c72013134ca29e81cd91a
compaq011 <tkyzlt99@gmail.com>
2025-07-20 21:58:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+const notes = [
+  "seni çok özledim 💘",
+  "Her günüm seninle daha güzel 🌞",
+  "sen benim herşeyimsiinn 😍"
+];
+
+function showNote(index) {
+  const noteDiv = document.getElementById('note');
+  noteDiv.innerText = notes[index];
+  noteDiv.style.display = 'block';
+}
